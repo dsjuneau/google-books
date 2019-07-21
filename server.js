3bd3740675d5df51bdb69bbc8e20750939f26565
Scott Juneau <dsjuneau@gmail.com>
2019-07-21 10:13:22 -0500

    fleshing out database connection

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,6 +19,11 @@ if (process.env.NODE_ENV === "production") {
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true
 });
 
 app.listen(PORT, () => {
